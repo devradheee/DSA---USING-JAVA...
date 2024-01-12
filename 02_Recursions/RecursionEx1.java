a2fa4c3cdec9cc09c5a72e8c9b5fd14fb221f35b
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-01-12 06:03:21 +0000

    Recursion example

---
+++ 02_Recursions/RecursionEx1.java
@@ -0,0 +1,18 @@
+public class RecursionEx1 {
+    static int x = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Hi Main");
+        m1();
+        System.out.println("Bye Main");
+    }
+
+    static void m1() {
+        System.out.println("Hello M1");
+        if (x <= 1) {
+            x++;
+            m1();
+        }
+        System.out.println("Bye M1");
+    }
+}
